99446475d8b491c67eecd544ac460e5846700daa
abdelrahman-abobakr <abdelrahmanabobakr321@gmail.com>
2025-04-28 17:12:28 +0300

    modifying connection files

--- Connection.php
+++ Connection.php
@@ -1,13 +1,14 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "Cafeteria";
-$port = 8111;
 
-$connection = mysqli_connect($servername, $username, $password, $dbname, $port);
-
-if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-?>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "Cafeteria";
+    
+    $connection  = mysqli_connect($servername, $username, $password, $dbname);
+    
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    ?>
+    
--- connect.php
+++ connect.php
@@ -1,13 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "Cafeteria";
-$port = 8111;
+    $serverName = "localhost";
+    $username = "root";
+    $pass = "";
+    $databaseName = "cafeteria";
 
-$connection = mysqli_connect($servername, $username, $password, $dbname, $port);
+    $connection = mysqli_connect($serverName, $username, $pass,$databaseName);
 
-if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 ?>
